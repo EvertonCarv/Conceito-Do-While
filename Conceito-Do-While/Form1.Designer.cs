b5702b4d70fc6538d34699531f556fecf2a8bc4e
Everton carvalho <everton.de.carvalho@hotmail.com.br>
2021-11-10 10:10:21 -0300

    Adicionar arquivos de projeto.

---
+++ Conceito-Do-While/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Conceito_Do_While
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTeste = new System.Windows.Forms.Button();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTeste
+            // 
+            this.btnTeste.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnTeste.Location = new System.Drawing.Point(68, 74);
+            this.btnTeste.Name = "btnTeste";
+            this.btnTeste.Size = new System.Drawing.Size(98, 55);
+            this.btnTeste.TabIndex = 0;
+            this.btnTeste.Text = "Teste Do While";
+            this.btnTeste.UseVisualStyleBackColor = true;
+            this.btnTeste.Click += new System.EventHandler(this.btnTeste_Click);
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(91, 25);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(0, 15);
+            this.lblNumero.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 180);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.btnTeste);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTeste;
+        private System.Windows.Forms.Label lblNumero;
+    }
+}
+
